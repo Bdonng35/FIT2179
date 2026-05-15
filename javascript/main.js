0cd236773d39b2f5344a631fe4fd8802665ddb38
Bdonng35 <bngg0029@student.monash.edu>
2026-05-15 12:01:27 +0800

    Added css styling, initial skeleton of dv2, and some charts

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+var vg_1 = "../charts/line_chart.json";
+vegaEmbed("#chart-income-time", vg_1).then(function(result) {
+    // Access the Vega view instance as result.view
+}).catch(console.error);
+
+var vg_2 = "../charts/household_growth.json";
+vegaEmbed("#chart-household-growth", vg_2).then(function(result) {
+    // Access the Vega view instance as result.view
+}).catch(console.error);
