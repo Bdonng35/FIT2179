1267f91ee7ac7db82e2637afb7967b0bc5d5b6a3
Bdonng35 <bngg0029@student.monash.edu>
2026-05-16 12:39:22 +0800

    Made changes so naming is aligned and added choropleth map

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,14 @@
-var vg_1 = "../charts/line_chart.json";
+var vg_1 = "charts/line_chart.json";
 vegaEmbed("#chart-income-time", vg_1).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
-var vg_2 = "../charts/household_growth.json";
+var vg_2 = "charts/household_growth.json";
 vegaEmbed("#chart-household-growth", vg_2).then(function(result) {
+    // Access the Vega view instance as result.view
+}).catch(console.error);
+
+var vg_3 = "charts/income_map.json";
+vegaEmbed("#chart-income-map", vg_3).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
