50138674474c6e2dd39ebefdfde0e716c2b8e71c
Bdonng35 <bngg0029@student.monash.edu>
2026-05-21 12:50:59 +0800

    Push finals changes before critique

--- javascript/main.js
+++ javascript/main.js
@@ -1,59 +1,61 @@
+var embedOptions = { actions: false };
+
 var vg_1 = "charts/line_chart.json";
-vegaEmbed("#chart-income-time", vg_1).then(function(result) {
+vegaEmbed("#chart-income-time", vg_1, embedOptions).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 var vg_2 = "charts/household_growth.json";
-vegaEmbed("#chart-household-growth", vg_2).then(function(result) {
+vegaEmbed("#chart-household-growth", vg_2, embedOptions).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 var vg_3 = "charts/income_map.json";
-vegaEmbed("#chart-income-map", vg_3).then(function(result) {
+vegaEmbed("#chart-income-map", vg_3, embedOptions).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 var vg_4 = "charts/income_ranking.json";
-vegaEmbed("#chart-income-ranking", vg_4).then(function(result) {
+vegaEmbed("#chart-income-ranking", vg_4, embedOptions).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 var vg_5 = "charts/poverty_map.json";
-vegaEmbed("#chart-poverty-map", vg_5).then(function(result) {
+vegaEmbed("#chart-poverty-map", vg_5, embedOptions).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 var vg_6 = "charts/income_poverty.json";
-vegaEmbed("#chart-income-poverty", vg_6).then(function(result) {
+vegaEmbed("#chart-income-poverty", vg_6, embedOptions).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 var vg_7 = "charts/gini_multi_linechart.json";
-vegaEmbed("#chart-gini-multiline", vg_7).then(function(result) {
+vegaEmbed("#chart-gini-multiline", vg_7, embedOptions).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 var vg_8 = "charts/income_gini.json";
-vegaEmbed("#chart-income-gini", vg_8).then(function(result) {
+vegaEmbed("#chart-income-gini", vg_8, embedOptions).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 var vg_9 = "charts/income_expenditure.json";
-vegaEmbed("#chart-income-expenditure", vg_9).then(function(result) {
+vegaEmbed("#chart-income-expenditure", vg_9, embedOptions).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 var vg_10 = "charts/expenditure_ranking.json";
-vegaEmbed("#chart-expenditure-ranking", vg_10).then(function(result) {
+vegaEmbed("#chart-expenditure-ranking", vg_10, embedOptions).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 var vg_11 = "charts/amenities_comparison.json";
-vegaEmbed("#chart-amenities-comparison", vg_11).then(function(result) {
+vegaEmbed("#chart-amenities-comparison", vg_11, embedOptions).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
 
 var vg_12 = "charts/percentile_distribution.json";
-vegaEmbed("#chart-percentile-distribution", vg_12).then(function(result) {
+vegaEmbed("#chart-percentile-distribution", vg_12, embedOptions).then(function(result) {
     // Access the Vega view instance as result.view
 }).catch(console.error);
